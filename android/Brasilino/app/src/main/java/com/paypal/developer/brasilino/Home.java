07ef791f8af8c4736fc87fd9a7ee12ea862e5d94
Jeff Prestes <jeffprestes@gmail.com>
2015-02-03 22:17:50 -0200

    Added permissions needed

--- android/Brasilino/app/src/main/java/com/paypal/developer/brasilino/Home.java
+++ android/Brasilino/app/src/main/java/com/paypal/developer/brasilino/Home.java
@@ -1,5 +1,7 @@
 package com.paypal.developer.brasilino;
 
+import android.app.Application;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageInfo;
@@ -20,8 +22,6 @@ import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.Toast;
 
-import com.paypal.developer.brasilino.util.Util;
-
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -38,11 +38,19 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
 
     private ImageView imgEsquerda;
     private ImageView imgDireita;
+    private ImageView imgCima;
+    private ImageView imgBaixo;
 
     private boolean frente = false;
     private boolean tras = false;
 
     private String ip;
+
+    private BufferedWriter escritorLinhas;
+    private OutputStreamWriter escritorCaracteres;
+    private OutputStream escritorSocket;
+
+    private Socket s;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,6 +59,12 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
 
         SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
         ip = sp.getString("ip", "");
+
+        if (ip.endsWith("tht")){
+            ip = ip.substring(0, ip.length() - 3);
+        }
+
+        Toast.makeText(this, ip, Toast.LENGTH_LONG).show();
 
         getSupportActionBar().hide();
 
@@ -77,10 +91,27 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
         imgDireita = (ImageView) findViewById(R.id.imgDireita);
         imgDireita.setOnTouchListener(this);
 
+        imgCima = (ImageView) findViewById(R.id.imgCima);
+        imgCima.setOnTouchListener(this);
+
+        imgBaixo = (ImageView) findViewById(R.id.imgBaixo);
+        imgBaixo.setOnTouchListener(this);
+
+        imgEsquerda.setEnabled(false);
+        imgBaixo.setEnabled(false);
+        imgCima.setEnabled(false);
+        imgDireita.setEnabled(false);
+
         WebView web = (WebView) findViewById(R.id.webview);
         WebSettings webSettings = web.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        web.loadUrl("http://"+ Util.retiraTHT(ip) +":8181/camera.php");
+        web.loadUrl("http://"+ip+":8181/camera.php");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        new CreateSocket().execute();
     }
 
     public void cima(View v) {
@@ -95,21 +126,25 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
     public boolean onTouch(View v, MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN){
             if (v == imgEsquerda) {
-                new Assincrono().execute("E;");
-            } else {
-                new Assincrono().execute("D;");
+                new Assincrono().execute("EA;");
+            } else if (v == imgDireita) {
+                new Assincrono().execute("DA;");
+            } else if (v == imgCima) {
+                new Assincrono().execute("FA;");
+            } else if (v == imgBaixo) {
+                new Assincrono().execute("TA;");
             }
-        }
-        /*
-        else if (event.getAction() == MotionEvent.ACTION_UP){
-
+         } else if (event.getAction() == MotionEvent.ACTION_UP){
             if (v == imgEsquerda) {
-                new Assincrono().execute("E;");
-            } else {
-                new Assincrono().execute("D;");
+                new Assincrono().execute("ED;");
+            } else if (v == imgDireita) {
+                new Assincrono().execute("DD;");
+            } else if (v == imgCima) {
+                new Assincrono().execute("FD;");
+            } else if (v == imgBaixo) {
+                new Assincrono().execute("TD;");
             }
         }
-         */
         return true;
     }
 
@@ -117,22 +152,11 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
 
         @Override
         protected String doInBackground(String... params) {
-            BufferedWriter escritorLinhas;
-            OutputStreamWriter escritorCaracteres;
-            OutputStream escritorSocket;
 
-            Socket s;
             try {
-                s = new Socket(Util.retiraTHT(ip), 8282);
-
-                escritorSocket = s.getOutputStream();
-                escritorCaracteres = new OutputStreamWriter(escritorSocket);
-                escritorLinhas = new BufferedWriter(escritorCaracteres);
                 escritorLinhas.write(params[0]);
                 escritorLinhas.flush();
-                escritorLinhas.close();
 
-                s.close();
                 return "foi";
             } catch (UnknownHostException e) {
                 // TODO Auto-generated catch block
@@ -145,5 +169,60 @@ public class Home extends ActionBarActivity implements View.OnTouchListener{
             }
         }
 
+    }
+
+    class CreateSocket extends AsyncTask<String, Void, String> {
+
+        private ProgressDialog pd;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            //pd = ProgressDialog.show(, getString(R.string.aviso), getString(R.string.corpo_aviso));
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                s = new Socket(ip, 8282);
+
+                escritorSocket = s.getOutputStream();
+                escritorCaracteres = new OutputStreamWriter(escritorSocket);
+                escritorLinhas = new BufferedWriter(escritorCaracteres);
+                //escritorLinhas.write(params[0]);
+                //escritorLinhas.flush();
+                //escritorLinhas.close();
+
+                //s.close();
+                return "foi";
+
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+                return "UnknownHostException: ";
+            } catch (IOException e) {
+                e.printStackTrace();
+                return "IOException: "+e;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            //pd.dismiss();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        try {
+            escritorLinhas.close();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        super.onDestroy();
     }
 }
