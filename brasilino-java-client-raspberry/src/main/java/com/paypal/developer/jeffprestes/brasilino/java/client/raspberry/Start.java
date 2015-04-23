ab7bc8ffaaeae77f9d99cc303daa531f51a09a2c
Jeff Prestes <jeffprestes@gmail.com>
2015-04-23 16:46:54 -0400

    Motor's checking on system initialization

--- brasilino-java-client-raspberry/src/main/java/com/paypal/developer/jeffprestes/brasilino/java/client/raspberry/Start.java
+++ brasilino-java-client-raspberry/src/main/java/com/paypal/developer/jeffprestes/brasilino/java/client/raspberry/Start.java
@@ -5,6 +5,9 @@
  */
 package com.paypal.developer.jeffprestes.brasilino.java.client.raspberry;
 
+import com.paypal.developer.jeffprestes.brasilino.java.client.raspberry.communication.SocketComm;
+import com.paypal.developer.jeffprestes.brasilino.java.client.raspberry.motor.Motor;
+import com.paypal.developer.jeffprestes.brasilino.java.client.raspberry.motorcontrollerboard.OseppMotorContoller;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -19,11 +22,14 @@ public class Start {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        SocketComm socketComm = null;
-        
-        socketComm = new SocketComm();
-        socketComm.socketStart();
-        
+        System.out.println("Activating motor...");
+        Motor m = new Motor(new OseppMotorContoller());
+        if (m.isOK())   {
+            System.out.println("Motor activated...");
+            SocketComm socketComm = null;
+            socketComm = new SocketComm(m);
+            socketComm.socketStart();
+        }
     }
     
 }
