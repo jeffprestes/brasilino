882c82dad8e0a9be49967898ee3838023238f032
Jeff Prestes <jeffprestes@gmail.com>
2015-04-23 16:27:50 -0400

    Segregate packages. This way MotorController boards can become an entire independent library.

---
+++ brasilino-java-client-raspberry/src/main/java/com/paypal/developer/jeffprestes/brasilino/java/client/motorcontrollerboard/L298NSimple.java
@@ -0,0 +1,20 @@
+package com.paypal.developer.jeffprestes.brasilino.java.client.motorcontrollerboard;
+
+import com.pi4j.io.gpio.RaspiPin;
+
+/**
+ * @author marabesi
+ */
+public class L298NSimple extends MotorController {
+
+    public L298NSimple() {
+        super();
+        this.setHasPWM(false);
+        /*this.setPinPWMDirecao(1);
+        this.setPinPWMPropulsao(1);*/
+        this.setPinDireita(RaspiPin.GPIO_07);
+        this.setPinEsquerda(RaspiPin.GPIO_00);
+        this.setPinFrente(RaspiPin.GPIO_02);
+        this.setPinTras(RaspiPin.GPIO_03);
+    }
+}
---
+++ brasilino-java-client-raspberry/src/main/java/com/paypal/developer/jeffprestes/brasilino/java/client/motorcontrollerboard/OseppMotorContoller.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.paypal.developer.jeffprestes.brasilino.java.client.motorcontrollerboard;
+
+import com.paypal.developer.jeffprestes.brasilino.java.client.motorcontrollerboard.MotorController;
+import com.pi4j.io.gpio.RaspiPin;
+
+/**
+ * Represents my Osepp Motor Controller board Pin Mapping suggested to RaspberryPi
+ * http://osepp.com/products/sensors-arduino-compatible/motor-driver/
+ * @author jprestes
+ */
+public class OseppMotorContoller extends MotorController {
+    
+    public OseppMotorContoller()    {
+        super();
+        this.setHasPWM(true);
+        this.setPinPWMDirecao(23);
+        this.setPinPWMPropulsao(1);
+        this.setPinDireita(RaspiPin.GPIO_02);
+        this.setPinEsquerda(RaspiPin.GPIO_03);
+        this.setPinFrente(RaspiPin.GPIO_05);
+        this.setPinTras(RaspiPin.GPIO_04);
+    }
+    
+}
